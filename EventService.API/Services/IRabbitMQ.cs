fe33698f0e60cce65311abb0841fcccda75aeec5
moroccomicrosoftcommunity <mormicrosoftcom@gmail.com>
2024-09-06 14:41:10 +0100

    Delete EventService.API directory

--- EventService.API/Services/IRabbitMQ.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EventService.API.Services
-{
-    public interface IRabbitMQ
-    {
-        public Task<object> RequestMethod(string Services, string Request);
-        public Task<object> DeleteMethod(string Services, string Request, Guid id);
-    }
-}
