8bb8c4418ccf3c1d96fe7c36ac51b0d07825298d
Tarik FANID <tarik.fanid@oumdin.com>
2024-09-26 15:13:32 +0100

    Update Table Event

--- EventService.Domain/Entities/Event.cs
+++ EventService.Domain/Entities/Event.cs
@@ -4,6 +4,7 @@
 public class Event
 {
     public Guid Id { get; private set; }
+    public string EventN { get; private set; }
     public string Title { get; private set; }
     public string? Address { get; private set; }
     public string? Description { get; private set; }
@@ -17,14 +18,16 @@ public class Event
     /// </summary>
     public int TypeEvent { get; private set; }
     public DateTime? EndDate { get; private set; }
-    public Guid CityId { get; private set; }
+    public Guid? CityId { get; private set; }
     public Guid? ProgramId { get; private set; } = null;
     public Program? Program { get; private set; }
     public Guid? ThemeId { get; private set; }
+    public string? LinkRegister { get; private set; }
+
 
     public ICollection<Session>? Sessions { get; private set; }
-    
-    public Event(string title, string? address, string? description, string? imagePath, DateTime? startDate, DateTime? endDate, Guid cityId, Guid? themeId)
+
+    public Event(string title, string? address, string? description, string? imagePath, DateTime? startDate, int typeEvent, string eventN, string? linkRegister, DateTime? endDate, Guid? cityId, Guid? themeId)
     {
         Id = Guid.NewGuid();
         Title = title;
@@ -35,5 +38,8 @@ public class Event
         EndDate = endDate;
         CityId = cityId;
         ThemeId = themeId;
+        TypeEvent = typeEvent;
+        LinkRegister = linkRegister;
+        EventN = eventN;
     }
 }
--- EventService.Infrastructure/Migrations/20240926121159_updateTableEvent.Designer.cs
+++ EventService.Infrastructure/Migrations/20240926121159_updateTableEvent.Designer.cs
@@ -4,6 +4,7 @@ using EventService.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventService.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240926121159_updateTableEvent")]
+    partial class updateTableEvent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -31,7 +34,7 @@ namespace EventService.Infrastructure.Migrations
                     b.Property<string>("Address")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<Guid?>("CityId")
+                    b.Property<Guid>("CityId")
                         .HasColumnType("uniqueidentifier");
 
                     b.Property<string>("Description")
