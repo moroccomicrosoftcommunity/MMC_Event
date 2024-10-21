0d2f0b7bfc87ed890da87c4d06e9e93f1cedf0e7
hakimoufkir <abdelhakimoufkir2000@gmail.com>
2024-10-21 10:29:28 +0100

    test4

--- EventService.Domain/Entities/Event.cs
+++ EventService.Domain/Entities/Event.cs
@@ -3,7 +3,7 @@
 
 public class Event
 {
-    //test 3
+    //test 4
     public Guid Id { get; private set; }
     public string EventN { get; private set; }
     public string Title { get; private set; }
