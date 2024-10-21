d845c1cfa7710cb05282bd6e36ac95a41b206f23
hakimoufkir <abdelhakimoufkir2000@gmail.com>
2024-10-21 12:48:48 +0100

    fixing endpoint fileUrls

--- EventService.API/Controllers/imagesController.cs
+++ EventService.API/Controllers/imagesController.cs
@@ -148,7 +148,7 @@ namespace SpeakerService.Api.Controllers
             {
                 List<string> urls = new List<string>();
 
-                foreach (BlobItem blobItem in _containerClient.GetBlobs(prefix: fileId))
+                foreach (BlobItem blobItem in _containerClient.GetBlobs(prefix: fileId.ToString()))
                 {
                     string fileName = blobItem.Name;
                     BlobClient blobClient = _containerClient.GetBlobClient(fileName);
