5a0fe42b55f5a1e20d8304e2478cf00975db44c2
hakimoufkir <abdelhakimoufkir2000@gmail.com>
2024-10-21 11:44:54 +0100

    fixing event entity

--- EventService.Domain/Entities/Event.cs
+++ EventService.Domain/Entities/Event.cs
@@ -4,45 +4,31 @@
 public class Event
 {
     //test 5
-    public Guid Id { get; private set; }
-    public string EventN { get; private set; }
-    public string Title { get; private set; }
-    public string? Address { get; private set; }
-    public string? Description { get; private set; }
+    public Guid Id { get;  set; }
+    public string? EventN { get;  set; }
+    public string? Title { get;  set; }
+    public string? Address { get;  set; }
+    public string? Description { get;  set; }
     public string? ImagePath { get; set; }
     public string?  ImageSliderlPath { get; set; }
     public string? ImageListEventPath { get; set; }
-    public DateTime? StartDate { get; private set; }
+    public DateTime? StartDate { get;  set; }
     /// <summary>
     /// Le type d'evenement
     /// Webinnaire 
     /// Hypered
     /// Pre
     /// </summary>
-    public int TypeEvent { get; private set; }
-    public DateTime? EndDate { get; private set; }
-    public Guid CityId { get; private set; }
-    public Guid? ProgramId { get; private set; } = null;
-    public Program? Program { get; private set; }
-    public Guid? ThemeId { get; private set; }
-    public string? LinkRegister { get; private set; }
+    public int? TypeEvent { get;  set; }
+    public DateTime? EndDate { get;  set; }
+    public Guid? CityId { get;  set; }
+    public Guid? ProgramId { get;  set; } = null;
+    public Program? Program { get;  set; }
+    public Guid? ThemeId { get;  set; }
+    public string? LinkRegister { get;  set; }
 
 
-    public ICollection<Session>? Sessions { get; private set; }
+    public ICollection<Session>? Sessions { get;  set; }
 
-    public Event(string title, string? address, string? description, string? imagePath, DateTime? startDate, int typeEvent, string eventN, string? linkRegister, DateTime? endDate, Guid cityId, Guid? themeId)
-    {
-        Id = Guid.NewGuid();
-        Title = title;
-        Address = address;
-        Description = description;
-        ImagePath = imagePath;
-        StartDate = startDate;
-        EndDate = endDate;
-        CityId = cityId;
-        ThemeId = themeId;
-        TypeEvent = typeEvent;
-        LinkRegister = linkRegister;
-        EventN = eventN;
-    }
+    
 }
---
+++ EventService.Infrastructure/Migrations/20241021102904_hamid.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class hamid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeEvent",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EventN",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CityId",
+                table: "Events",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeEvent",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EventN",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CityId",
+                table: "Events",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
