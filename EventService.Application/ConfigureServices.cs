de00b1e4d58d8a5f79d49ff6636509b89fee74f2
oussamaettaoui <att.oussama.pypl@gmail.com>
2024-10-23 14:39:55 +0100

    commit

--- EventService.Application/ConfigureServices.cs
+++ EventService.Application/ConfigureServices.cs
@@ -1,47 +1,35 @@
-﻿using EventServices.Application.Interfaces;
+﻿using EventService.Application.Interfaces;
+using EventService.Application.Services;
+using EventServices.Application.Interfaces;
 using EventServices.Application.Mapping;
-using EventServices.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
-public static class ConfigureServices
+
+namespace EventServices.Application.Services
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static class ConfigureServices
     {
-        // AutoMapper registration
-        services.AddAutoMapper(typeof(AutoMapperProfile));
-
-        // MediatR registration
-        services.AddMediatR(config =>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-        });
+            // AutoMapper registration
+            services.AddAutoMapper(typeof(AutoMapperProfile));
 
-        // Services Injection
-        services.AddScoped<IUnitOfService, UnitOfService>();
-        services.AddTransient<IBlobService, BlobService>();
-        services.AddTransient<ISessionService, SessionService>();
-        services.AddTransient<IProgramService, ProgramService>();
-        services.AddTransient<IEventService, EventService>();
-
-<<<<<<< HEAD
-        // Broker Injection - Use a factory method to resolve IUnitOfService
-        services.AddSingleton<RabbitMQServer>(serviceProvider =>
-        {
-            var unitOfService = serviceProvider.GetRequiredService<IUnitOfService>();
-            return new RabbitMQServer(unitOfService);
-        });
-=======
+            // MediatR registration
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
             //Services Injection
             services.AddScoped<IUnitOfService, UnitOfService>();
             services.AddScoped<ISliderService, SliderService>();
-            services.AddScoped<IEventService, Services.EventService>();
+            services.AddScoped<IEventService, EventService>();
             services.AddScoped<IProgramService, ProgramService>();
             services.AddScoped<ISessionService, SessionService>();
             //Broker Injection
             var AlltheServices = services.BuildServiceProvider().GetService<IUnitOfService>();
->>>>>>> a18fd5604255217c475018b9cb6419b7aaa6a2a1
 
-        return services;
+            return services;
+        }
     }
 }
